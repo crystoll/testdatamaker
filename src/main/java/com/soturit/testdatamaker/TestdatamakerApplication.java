ba47aa7254f91c0da9e7dbb96568f755c867c631
Arto Santala <crystoll@gmail.com>
2021-03-26 13:35:13 +0200

    Initial import. Minimal Spring boot + REST example.

---
+++ src/main/java/com/soturit/testdatamaker/TestdatamakerApplication.java
@@ -0,0 +1,13 @@
+package com.soturit.testdatamaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdatamakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestdatamakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soturit/testdatamaker/TestdatamakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soturit.testdatamaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdatamakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
