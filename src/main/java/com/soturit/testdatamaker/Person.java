98ba07e62da6c20222871459aa49ba2fc8dcae97
Arto Santala <crystoll@gmail.com>
2021-04-01 14:50:05 +0300

    Refactored Person class from boring old class to hot new Java 16 record.

--- src/main/java/com/soturit/testdatamaker/Person.java
+++ src/main/java/com/soturit/testdatamaker/Person.java
@@ -2,57 +2,9 @@ package com.soturit.testdatamaker;
 
 import java.util.Objects;
 
-public class Person {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-	private final String firstName;
-	private final String lastName;
-	private final String email;
-
-
-	public Person(String firstName, String lastName, String email) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
-
-	public String getFirstName() {
-		return this.firstName;
-	}
-
-
-	public String getLastName() {
-		return this.lastName;
-	}
-
-
-	public String getEmail() {
-		return this.email;
-	}
-
-
-	@Override
-	public boolean equals(Object o) {
-		if (o == this)
-			return true;
-		if (!(o instanceof Person)) {
-			return false;
-		}
-		Person person = (Person) o;
-		return Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(email, person.email);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(firstName, lastName, email);
-	}
-
-	@Override
-	public String toString() {
-		return "{" +
-			" firstName='" + getFirstName() + "'" +
-			", lastName='" + getLastName() + "'" +
-			", email='" + getEmail() + "'" +
-			"}";
-	}
-
-}
+public record Person(
+	@JsonProperty("firstName") String firstName, 
+	@JsonProperty("lastName") String lastName, 
+	@JsonProperty("email") String email) {}
