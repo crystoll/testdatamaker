a61d22130d230f4a710f39ad5028ea61034f1cf2
Arto Santala <crystoll@gmail.com>
2021-07-24 17:14:48 +0300

    Fixing and preparing a bit for the docker video

---
+++ src/test/java/com/soturit/testdatamaker/TestdatamakerApplicationTest.java
@@ -0,0 +1,12 @@
+package com.soturit.testdatamaker;
+
+import org.junit.jupiter.api.Test;
+
+class TestdatamakerApplicationTest {
+
+	@Test
+	void placeholder() {
+		System.out.println("Unit test run!");
+	}
+
+}
